23bdd7956f7eb6bd0a03b36c4f70c117069631d9
yash27op <118678900+yash27op@users.noreply.github.com>
2025-05-02 16:38:03 +0530

    Update create-pr.sh

--- scripts/create-pr.sh
+++ scripts/create-pr.sh
@@ -1,1 +1,9 @@
-#set -euo pipefail
+#!/bin/bash
+
+echo "--------------------------------------"
+echo " Hello from myscript.sh running inside Tekton!"
+echo " Current Date and Time: $(date)"
+echo " Current Working Directory: $(pwd)"
+echo " List of Files:"
+ls -l
+echo "--------------------------------------"
